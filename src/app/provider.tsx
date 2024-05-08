330737326be3d8234f6989b631fd45a9df8ce86c
kyh <kyh_0625@naver.com>
2024-05-09 01:45:17 +0900

    nextauth

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from 'recoil';
 
 interface NextProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function NextProvider({children}: NextProviderProps) {
+  const { session } = NextProviderProps;
   return (
     <RecoilRoot>
+      <SessionProvider session={session}>
       {children}
+      </SessionProvider>
     </RecoilRoot>
   );
 }
